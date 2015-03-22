4a3992d5bac06df88e2da4b9d1e4095675a655e4
Henric Andersson <ha@sensenet.nu>
2015-03-21 17:54:11 -0700

    Added initial IR driver

---
+++ driver_basicir.py
@@ -0,0 +1,73 @@
+"""
+Base IR driver, most of the time, this is what you need.
+"""
+
+from driver_null import DriverNull
+import requests
+import base64
+import json
+from commandtype import CommandType
+
+class DriverBasicIR(DriverNull):
+  ircmds = None
+  code_on = "on"
+  code_off = "off"
+  server = None
+
+  def __init__(self, server, commandfile):
+    DriverNull.__init__(self)
+
+    jdata = open(commandfile)
+    self.ircmds = json.load(jdata)
+    if not "on" in self.ircmds:
+      print "INFO: Using toggle for %s instead of discreet on/off" % commandfile
+      code_on = code_off = "toggle"
+    self.server = server
+
+    """
+    By default, this driver will prefill the commandlist with PRIVATE_UNDEFINED, this way
+    a driver based on this can easily fill in the gaps.
+    """
+    for cmd in self.ircmds:
+      if cmd == "on" or cmd == "off" or cmd == "toggle":
+        continue
+      self.COMMAND_HANDLER[cmd] = {
+        "arguments"   : 0, 
+        "handler"     : self.sendCommand, "extras" : cmd,
+        "name"        : "Undefined",
+        "description" : "Undefined",
+        "type"        : CommandType.PRIVATE_UNDEFINED
+      }
+
+  def setPower(self, enable):
+    """
+    We need to override this and use the on/off pair or toggle to handle
+    power.
+    """
+    if self.power == enable:
+      return True
+
+    if enable:
+      self.sendIr(self.code_on)
+    else:
+      self.sendIr(self.code_off)
+
+    self.power = enable
+    return True
+
+  def sendCommand(self, zone, command):
+    self.sendIr(command)
+
+  def sendIr(self, command):
+    if not command in self.ircmds:
+      print "WARN: %s is not a defined IR command" % command
+
+    ir = self.ircmds[command]
+
+    url = self.server + "/write/" + ir
+    r = requests.get(url)
+    if r.status_code != 200:
+      print "ERROR: Driver was unable to execute %s" % url
+      return False
+
+    j = r.json()
