057665829cc3dc51ee48eb68d0f5a0117a9837fa
Henric Andersson <ha@sensenet.nu>
2015-03-24 21:08:24 -0700

    Added additional command types
    Basic usage of system has revealed the need for additional command
    types. Also renumbered the types to give more headroom for expansion.

--- commandtype.py
+++ commandtype.py
@@ -5,30 +5,44 @@ class CommandType:
   VOLUME_UNMUTE   = 4
   VOLUME_SET      = 5   # Takes argument, 0-100 = 0-100% volume, 100-150 = 0-50% above 0dB
 
-  PLAYBACK_PLAY         = 10
-  PLAYBACK_PAUSE        = 11
-  PLAYBACK_STOP         = 11
-  PLAYBACK_NEXT         = 12 
-  PLAYBACK_PREVIOUS     = 13 
-  PLAYBACK_CNEXT        = 14 
-  PLAYBACK_CPREVIOUS    = 15 
-  PLAYBACK_FASTFORWARD  = 16 
-  PLAYBACK_REWIND       = 17 
+  PLAYBACK_PLAY           = 100
+  PLAYBACK_PAUSE          = 101
+  PLAYBACK_STOP           = 101
+  PLAYBACK_NEXT           = 102 
+  PLAYBACK_PREVIOUS       = 103 
+  PLAYBACK_CNEXT          = 104 
+  PLAYBACK_CPREVIOUS      = 105 
+  PLAYBACK_FASTFORWARD    = 106 
+  PLAYBACK_REWIND         = 107
 
-  NAVIGATE_UP           = 20 
-  NAVIGATE_DOWN         = 21
-  NAVIGATE_LEFT         = 22
-  NAVIGATE_RIGHT        = 23
-  NAVIGATE_ENTER        = 24
-  NAVIGATE_BACK         = 25
-  NAVIGATE_HOME         = 26
-  NAVIGATE_MENU         = 27
-  NAVIGATE_TOPMENU      = 28
-  NAVIGATE_PAGEUP       = 29
-  NAVIGATE_PAGEDOWN     = 30
+  PLAYBACK_SKIP_FORWARD   = 108 # Usually 15s
+  PLAYBACK_SKIP_BACKWARD  = 109 # Usually 15s
+  PLAYBACK_LSKIP_FORWARD  = 110 # Usually 30s
+  PLAYBACK_LSKIP_BACKWARD = 111 # Usually 30s
+
+  PLAYBACK_STREAM         = 112
+  PLAYBACK_AUDIO          = 113
+  PLAYBACK_SUBTITLE       = 114
+
+  PLAYBACK_SHUFFLE        = 115
+  PLAYBACK_REPEAT         = 116
+
+  PLAYBACK_OSD            = 117
+
+  NAVIGATE_UP           = 200 
+  NAVIGATE_DOWN         = 201
+  NAVIGATE_LEFT         = 202
+  NAVIGATE_RIGHT        = 203
+  NAVIGATE_ENTER        = 204
+  NAVIGATE_BACK         = 205
+  NAVIGATE_HOME         = 206
+  NAVIGATE_MENU         = 207
+  NAVIGATE_TOPMENU      = 208
+  NAVIGATE_PAGEUP       = 209
+  NAVIGATE_PAGEDOWN     = 210
 
   # Private commands aren't meant to be exposed to end-user
-  PRIVATE_INPUT         = 100
+  PRIVATE_INPUT         = 900
 
   # Undefined should NEVER be used, but are handy for automatic prefill
   PRIVATE_UNDEFINED     = 10000
